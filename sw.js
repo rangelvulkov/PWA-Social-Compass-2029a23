f0abeba59b9f39ae6f360951d19fa006c357d3be
rangelvulkov <rangelvulkov1@gmail.com>
2026-01-19 19:33:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
+const CACHE_NAME = 'social-compass-v1';
+const assets = ['./', './index.html', './manifest.webmanifest'];
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('v1').then((cache) => cache.addAll(['index.html', 'manifest.webmanifest']))
-  );
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
